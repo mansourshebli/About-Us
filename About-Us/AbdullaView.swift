b4129a89d189e2c7b0597747d76cca790c3aa605
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-08-14 16:46:58 +0400

    Adding instructions

--- About-Us/AbdullaView.swift
+++ About-Us/AbdullaView.swift
@@ -5,6 +5,22 @@
 //  Created by Mansour Mohammed Alshebli on 14/08/2023.
 //
 
+
+/* Customize ur view the way u like after that, replace it with the text() view;
+Text("Abdulla")
+    .tabItem {
+        Label("Abdulla", systemImage: "person")
+    }
+CHANGE IT TO:
+ 
+AbdullaView()
+     .tabItem {
+         Label("Abdulla", systemImage: "person")
+     }
+ 
+ THE CHANGE SHOULD HAPPEN IN ContentView()
+ */
+
 import SwiftUI
 
 struct AbdullaView: View {
